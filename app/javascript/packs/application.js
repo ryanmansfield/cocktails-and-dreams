dfe2860f3761e94e4e31c49b6cd6f6413f1b9416
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-05-03 20:53:51 -0500

    filter working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,18 @@
 import "bootstrap";
+
+  var checkBoxes = document.querySelectorAll(".form-check-input");
+  var form = document.querySelector('form');
+
+  for (const check of checkBoxes) {
+  check.addEventListener( 'change', function() {
+    Rails.fire(form, 'submit');
+  });
+  }
+
+
+
+function replaceCocktails (innerHTML) {
+  const cocktails = document.getElementById('cocktails');
+  cocktails.innerHTML = innerHTML;
+}
+replaceCocktails("<%= j render 'cocktail', cocktails: @cocktails %>");
