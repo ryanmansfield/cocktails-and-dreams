60de488c98aeef7264b7963cf2f84da472a7497a
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-05-03 19:54:26 -0500

    actions added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,43 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    # @cocktails = Cocktail.all
+    if params[:query].present?
+      @search = "#{params[:query][:search]} #{params[:query][:filter]}"
+      @cocktails = Cocktail.search_by_name(@search)
+    else
+      @cocktails = Cocktail.all
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:cocktail).permit(:name, :photo)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,45 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, except: :destroy
+  before_action :set_dose, only: [:destroy, :show]
+
+  def new
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # @dose.ingredient = Ingredient.find(params['dose']['ingredient'])
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to @dose.cocktail
+  end
+
+  private
+
+  def dose_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 end
