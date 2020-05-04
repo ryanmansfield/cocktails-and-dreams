56270fb7b31c61d7839d36f52caa12661803032c
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-05-03 19:59:11 -0500

    views added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,10 +4,10 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true, allow_blank: false
   has_one_attached :photo
 
-  # include PgSearch::Model
-  # pg_search_scope :search_by_name,
-  #   against: [ :name ],
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
