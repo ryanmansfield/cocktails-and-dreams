8a824f8b834e9b1f9ad7aae298c4e26ccf040f91
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-05-05 22:49:51 -0500

    rating added to cocktail

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,25 @@
 class RatingsController < ApplicationController
+  before_action :set_cocktail
+
+  def create
+    @rating = Rating.new(rating_params)
+    @rating.cocktail = @cocktail
+
+    if @rating.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      flash[:alert] = "Something went wrong."
+      redirect_to cocktail_path(@cocktail)
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:rating)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
